9187e3e2cccf535b370e5e9d35ce99b9726a89b8
AiMoeNalu <48229683+AiMoeNalu@users.noreply.github.com>
2019-11-20 11:32:21 -0500

    Header and Nav bar svg and styling fixed. Need to design logo now and options

--- src/components/Social-Media/Icon-Links.jsx
+++ src/components/Social-Media/Icon-Links.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 import './Icon-Links.scss';
 
+import SOCIAL_MEDIA_DATA from '../../assets/Social-Media/Social-Media-Data';
+
 const IconLinks = () => (
     <div className='social-icons'>
-        
+        {
+            SOCIAL_MEDIA_DATA.map(({Name, Link, Icon}) => (
+                <div onClick={() => window.open(Link, "_blank")}>{Icon}</div>
+            ))
+        }
     </div>
 );
 
