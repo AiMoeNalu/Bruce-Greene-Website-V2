17b41f2c069a8b0fd33edb95be7dcda8fa74af7c
AiMoeNalu <48229683+AiMoeNalu@users.noreply.github.com>
2019-12-02 11:02:35 -0500

    Currency Input complete. Just need to style it.

--- src/components/Donate-Amount-Input/DonateAmountInput.jsx
+++ src/components/Donate-Amount-Input/DonateAmountInput.jsx
@@ -3,8 +3,11 @@ import CurrencyInput from 'react-currency-input';
 
 import './DonateAmountInput.scss';
 
-const DonateAmountInput = () => (
-    <CurrencyInput id='donate-amount-input' precision='2'/>
+
+//! Prefix comes from currency select (i.e. '$' for USD)
+
+const DonateAmountInput = ({prefix='$'}) => (
+    <CurrencyInput id='donate-amount-input' prefix={prefix} precision='2' value='2.00'/>
 );
 
 export default DonateAmountInput;
